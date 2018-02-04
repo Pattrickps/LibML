39be9a4bb8c59303412e0a776883646e023e08d6
Suraj Pai <surajballambat@gmail.com>
2018-02-04 11:59:36 +0530

    Add docstring for functions and structure for implementation

--- Linear/LinearRegression.py
+++ Linear/LinearRegression.py
@@ -1,2 +1,28 @@
+class LinearRegression:
+    def train(self, x, y):
+        """
+
+        :param x: Numpy array of features
+        :param y: Numpy array of labels
+        :return: None
+        """
+
+    def evaluate(self, x):
+        """
+
+        :param x: Numpy array of features
+        :return: Numpy array of predicted labels
+        """
+
+
+def gradient_descent_iteration(b, x, y):
+    """
+
+    :param b: Weights numpy array
+    :param x: Features numpy array
+    :param y: Labels numpy array
+    :return: Weights numpy array
+    """
+
 if __name__ == "__main__":
-	print("Linear Regression Model Class")
+    print("Linear Regression Model Class")
