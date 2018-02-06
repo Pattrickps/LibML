7dc1b875a624b6066828b45e2910445b744624e3
surajpaib <suraj.pai@cognitivemachines.co.in>
2018-02-06 13:49:34 +0530

    Added dummy variables to Linear Regression Class

--- Regression/LinearRegression.py
+++ Regression/LinearRegression.py
@@ -1,4 +1,12 @@
+import numpy as np
+
 class LinearRegression:
+    def __init__(self):
+        self.b = None # Weights numpy array [ Updated during training ]
+        self.x = np.zeros((100, 10)) # Default shape of 100 x 10 [ 100 rows and 10 features ]
+        self.y = np.zeros((100)) # Default shape of 100 [ 100 labels]
+        print("Initialized x array of shape: {0} and y array of shape: {1}".format(self.x.shape, self.y.shape))
+
     def train(self, x, y):
         """
 
@@ -25,4 +33,5 @@ def gradient_descent_iteration(b, x, y):
     """
 
 if __name__ == "__main__":
+    lr_instance = LinearRegression()
     print("Linear Regression Model Class")
